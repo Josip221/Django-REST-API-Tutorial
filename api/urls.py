337f11934544d18fde975406d2436c0acd79e431
Josip Maretić <josip.maretic2011@outlook.com>
2023-03-26 16:01:41 +0200

    api tutorial complete

---
+++ api/urls.py
@@ -0,0 +1,45 @@
+"""api URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, include
+from snippets import views
+from snippets.views import SnippetViewSet, UserViewSet, api_root
+from rest_framework.routers import DefaultRouter
+
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
+
+urlpatterns = [
+    path('', views.api_root),
+    path('snippets/', snippet_list, name="snippet-list"),
+    path('snippets/<int:pk>/', snippet_detail),
+    path('users/', user_list, name="user-list"),
+    path('users/<int:pk>/', user_detail),
+    path('api-auth/', include('rest_framework.urls')),
+]
